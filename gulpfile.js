98386069cc5481e73d4f04ecd6b6f1324be2f5ed
Dmitry Yakimov <welldan97@gmail.com>
2015-12-15 14:50:58 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/*global require*/
+
+var gulp = require('gulp');
+
+var html5Lint = require('gulp-html5-lint');
+var csslint = require('gulp-csslint');
+
+gulp.task('html5-lint', function() {
+  return gulp.src('*.html')
+    .pipe(html5Lint());
+});
+
+gulp.task('csslint', function() {
+  gulp.src('assets/*.css')
+    .pipe(csslint({
+      'box-model': false
+    }))
+    .pipe(csslint.reporter());
+});
+
+
+gulp.task('test', ['html5-lint', 'csslint']);
+gulp.task('default', ['test']);
